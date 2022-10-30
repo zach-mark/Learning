2dff822f232a3c5c39e4fa6d3941071855fe17aa
Zach Mark <zach.mark@protonmail.com>
2022-10-30 19:54:07 -0400

    Create Climbing Stairs.py
    Working on Solutions to the Climbing Stairs problem from leetcode.com

---
+++ Coding Problem Solutions/Climbing Stairs.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 30 17:56:17 2022
+
+@author: Zachary Mark
+"""
+
+"""
+https://leetcode.com/problems/climbing-stairs/
+
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps.
+In how many distinct ways can you climb to the top?
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+
+Constraints:
+
+1 <= n <= 45
+"""
+import copy
+
+def brute_climber(n):
+    #it works, but it ain't pretty
+    
+    #n= number of steps
+    #z= number of options
+    
+    routes=[[0]]
+    
+    complete=False
+    cycle=0
+    while complete==False:
+        for route in routes:
+            if route[-1]<n-1:
+                copy_route=copy.deepcopy(route)
+                copy_route.append(route[-1]+2)
+                routes.append(copy_route)
+                route.append(route[-1]+1)
+            elif route[-1]<n:
+                route.append(route[-1]+1)
+        
+        done=True
+        for route in routes:
+            if route[-1]!=n:
+                done=False
+        if done==True:
+            complete=True
+        
+        cycle+=1
+        print(cycle)
+            
+    
+    z=len(routes)
+    
+    print("Input = ",n)
+    print("Output = ",z)
+    print("Explanation: there are ",z," ways to climb to the top")
+    for route in routes:
+        print(route)
+    
