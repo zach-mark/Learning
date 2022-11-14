a5817ba2bf3c68192ea51def9bd9f5892f7017d7
Zach Mark <zach.mark@protonmail.com>
2022-11-13 19:24:31 -0500

    Completed Lesson 31

--- Automate the Boring Stuff w Python Programming- Udemy/30 Section 11 Files- Filenames and paths.py
+++ Automate the Boring Stuff w Python Programming- Udemy/30 Section 11 Files- Filenames and paths.py
@@ -6,7 +6,7 @@ Created on Thu Nov 10 22:25:34 2022
 """
 
 """
-Lesson 30
+Lesson 30 and 31
 
 Section 11: Files.
 
@@ -36,3 +36,50 @@ functions:
     getsize()- returns size in bits
 
 """
+
+
+
+
+
+
+
+#text editing.
+#bin, txt, py files.
+
+hello_file=open('C:\\Users\\zsm54\\Documents\\GitHub\\Learning\\Automate the Boring Stuff w Python Programming- Udemy\\hello.txt')
+content=hello_file.read()
+list_content=hello_file.readlines()
+
+hello_file.close()
+
+new_file= open('C:\\Users\\zsm54\\Documents\\GitHub\\Learning\\Automate the Boring Stuff w Python Programming- Udemy\\files_text_example_00.txt', 'w')
+
+for i in range(0,3):
+    new_file.write('Hello!!!!! \n')
+new_file.write('Goodbye.')
+new_file.close()
+
+
+new_file_2=open("files_text_example_01.txt", "w")
+new_file_2.write("Bacon is not a vegetable")
+new_file_2.close()
+
+new_file_2=open('bacon.txt', 'a')
+new_file_2.write("\n\nBacon is delicious.")
+new_file_2.close()
+
+
+
+"""
+the shelve module
+"""
+import shelve
+
+shelfFile=shelve.open('mydata')
+shelfFile['cats'] = ["Zophie", "Pooka", "Simon", "Fat-Tail", "Cleo"]
+shelfFile.close()
+
+#very similar to dictionaries
+
+
+
